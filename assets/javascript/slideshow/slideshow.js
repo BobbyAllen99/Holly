a01546947c7e5ce8cb3f8f5b5ccaf2320c868d3f
BobbyAllen99 <bobbyallen13@outlook.com>
2015-06-30 16:51:56 +0100

    Started

---
+++ assets/javascript/slideshow/slideshow.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+  $("#slideshow").owlCarousel({
+ 
+      navigation : false,
+      slideSpeed : 300,
+      pagination : false,
+      singleItem:true,
+      autoPlay : true,
+      transitionStyle : "fade"
+ 
+  });
+ 
+});
