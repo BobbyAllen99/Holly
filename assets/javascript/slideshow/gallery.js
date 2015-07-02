31c16bd0fae3b68000cde7b2527b90a3e3ad7d50
BobbyAllen99 <bobbyallen13@outlook.com>
2015-07-02 15:03:07 +0100

    test

--- assets/javascript/slideshow/gallery.js
+++ assets/javascript/slideshow/gallery.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
       items : 3,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3],
-      pagination : false,
+      pagination : true,
+      autoHeight : true,
  
   });
  
