0c2a19ab255d3c3179ab4c403f52386756862539
BobbyAllen99 <bobbyallen13@outlook.com>
2015-07-06 13:18:45 +0100

    changes

--- assets/javascript/scroll.js
+++ assets/javascript/scroll.js
@@ -1,14 +1,29 @@
-(function($) {
-    "use strict"; // Start of use strict
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 500
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a[href*=#]:not([href=#])').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+// (function($) {
+//     "use strict"; // Start of use strict
+
+//     // jQuery for page scrolling feature - requires jQuery Easing plugin
+//     $('a[href*=#]:not([href=#])').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: ($($anchor.attr('href')).offset().top - 100)
+//         }, 1250, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
 
 
-})(jQuery); // End of use strict
+// })(jQuery); // End of use strict
